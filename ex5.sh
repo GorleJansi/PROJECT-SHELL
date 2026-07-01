e73d2505a1a0284673754635c5c3438d37109cd9
Jansi Gorle <jansigorle@Jansis-MacBook-Air.local>
2026-07-01 17:05:56 +0530

    logs added to shell files

--- ex5.sh
+++ ex5.sh
@@ -2,30 +2,32 @@
 red=$'\e[31m'
 green=$'\e[32m'
 normal=$'\e[0m'
-
+folder=/var/log/shell-logs
+logfile=$(echo "$0" | cut -d "." -f 1).log
+mkdir -p $folder
 read -p "Enter package to install : " toinstall
 user=$(id -u)
 
 rootaccess(){
     if [ "$user" -ne 0 ]; then
-        echo -e "${red}ERROR${normal}: Please run this script as root."
+        echo -e "${red}ERROR${normal}: Please run this script as root." | tee -a logfile
         exit 1
     fi
 }
 
 validate(){
     if [ "$1" -ne 0 ]; then
-        echo -e "${red}ERROR${normal}: Failed to install $2"
+        echo -e "${red}ERROR${normal}: Failed to install $2" | tee -a logfile
         exit 1
     else
-        echo -e "${green}SUCCESS${normal}: $2 is installed"
+        echo -e "${green}SUCCESS${normal}: $2 is installed" | tee -a logfile
     fi
 }
 
-if dnf list installed "$toinstall" &> /dev/null; then
-    echo -e "${green}SUCCESS${normal}: $toinstall is already installed"
+if dnf list installed "$toinstall" &>> logfile; then
+    echo -e "${green}SUCCESS${normal}: $toinstall is already installed" | tee -a logfile
 else
     rootaccess
-    dnf install "$toinstall" -y &> /dev/null
+    dnf install "$toinstall" -y &>> logfile
     validate $? "$toinstall"
 fi
